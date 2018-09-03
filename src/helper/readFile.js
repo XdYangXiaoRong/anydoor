9f1b88f959ba31650775fed287c92ab9b3f6ac2f
yangxiaorong <xd_yangxiaorong@163.com>
2018-09-03 14:53:46 +0800

    html拼接，点击页面跳转到相应文档

--- src/helper/readFile.js
+++ src/helper/readFile.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const promisify = require('util').promisify;
 const stat = promisify(fs.stat);//stat方法promise化
 const readdir = promisify(fs.readdir);
+const config = require('../config/defaultConfig.js');
 module.exports = async function(req, res, filePath){
     try{
         const stats = await stat(filePath);//awit必须包含在async函数中,所以把所有代码移到定义的readFile函数中
@@ -13,8 +14,15 @@ module.exports = async function(req, res, filePath){
             //将readdir方法也promise化
             const files = await readdir(filePath);
                 res.statusCode = 200;
-                res.setHeader('Content-Type','text/plain');
-                res.end(files.join(','));//返回所有的文件名
+                res.setHeader('Content-Type','text/html;charset=UTF-8');
+                res.write('<html><body><div>')
+                var html= '';
+                for(let i=0;i<files.length;i++){//
+                    html+='<a style="display:block" href="http://127.0.0.1:9527'+ 
+                            req.url+'/'+files[i]+'">'+files[i]+'</a>';
+                }
+                res.write(html);//返回所有的文件名
+                res.end('</div></body></html>')
         }
     }catch(err){
         console.log(err)
@@ -22,6 +30,5 @@ module.exports = async function(req, res, filePath){
         res.setHeader('Content-Type','text/javascript;charset=UTF-8');//utf8编码，防止中文乱码
         res.end(`${filePath} is not a directory or file.`)
         return;
-        
     }
 }
